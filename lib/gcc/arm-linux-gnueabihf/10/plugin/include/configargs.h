f2033961444f249582cec0b6023b9792a95ede5e
Lars Krüger <user@mail.fake>
2022-10-04 08:56:33 +0200

    add raspberry-gcc10.2.1

---
+++ lib/gcc/arm-linux-gnueabihf/10/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../../src/gcc-10-10.2.1/src/configure --with-pkgversion='Raspbian 10.2.1-6+rpi1' --enable-languages=c,c++,lto --with-gcc-major-version-only --enable-shared --enable-linker-build-id --without-included-gettext --enable-threads=posix --enable-nls --enable-bootstrap --enable-clocale=gnu --enable-libstdcxx-debug --enable-libstdcxx-time=yes --with-default-libstdcxx-abi=new --enable-gnu-unique-object --disable-libitm --disable-libquadmath --disable-libquadmath-support --enable-plugin --enable-libphobos-checking=release --enable-objc-gc=auto --enable-multiarch --disable-sjlj-exceptions --with-arch=armv6 --with-fpu=vfp --with-float=hard --disable-werror --enable-checking=release --target=arm-linux-gnueabihf --with-sysroot=/mnt/f/gnu/raspberry-bullseye/out/arm-linux-gnueabihf/sysroot --build=x86_64-linux-gnu --disable-nls --host=x86_64-w64-mingw32 --prefix /mnt/f/gnu/raspberry-bullseye/out/";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "arm10e" }, { "arch", "armv6" }, { "float", "hard" }, { "fpu", "vfp" }, { "tls", "gnu" } };
